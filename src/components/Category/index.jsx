bbd8691506384c466c24d1441a6d4585a49ecb1e
Gabriel G <luisggm33@gmail.com>
2021-09-23 20:01:07 -0600

    category bug

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -5,7 +5,7 @@ import { Link, Image } from './styles';
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg';
 
 const Category = ({ cover = DEFAULT_IMAGE, path = '#', emoji = '?' }) => (
-  <Link to={path}>
+  <Link to={path} onClick={() => window.scrollTo(0, 0)}>
     <Image src={cover} />
     {emoji}
   </Link>
